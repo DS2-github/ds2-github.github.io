cd88c2c3ada124f472f5b412c2045ae49aead294
DS2 <smirnov.daniil.ds2@gmail.com>
2021-06-16 16:49:23 +0300

    quizes and socket-cookie-chat-server

--- Chat-socket/index.js
+++ Chat-socket/index.js
@@ -6,19 +6,48 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const port = 8000;
 
-let recoveryDATA = "";
+let recoveryDATA = [];
+let usernameLIST = ["DS2", "ds2"];
+let passwordLIST = ["cgsgforever", "CGSGFOREVER"];
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
+  /* Send message callback */
   socket.on('sendMessage', msg => {
-    let result = msg.split(":");
-    recoveryDATA = recoveryDATA + `<tr class="anim"><td class="name">${result[0]}:</td><td class="msg">${result[1]}</td></tr>`;
+    recoveryDATA.push(msg);
     io.emit('getMessage', msg);
   });
+
+  /* Request data if page reloaded */
   socket.on('requesRrecovery', code => {
     if (code === "code:30" && recoveryDATA != "")
       io.emit('receiveRecovery', recoveryDATA);
+  })
+
+  /* Registration of user */
+  socket.on('setUsrename', data => {
+    if (usernameLIST.indexOf(data.login) > -1)
+      socket.emit('usernameOccupied', data.login + ' username is taken! Try another username.');
+    else {
+      usernameLIST.push(data.login);
+      passwordLIST.push(data.password);
+      socket.emit('registerUser', { username: data.login });
+    }
+  })
+
+  /* Logging in user */
+  socket.on('getUsrename', data => {
+    let loginInd = usernameLIST.indexOf(data.login);
+    if (loginInd == -1) {
+      socket.emit('loginError', data.login + ' -- no such user exist.');
+    } else {
+      if (passwordLIST[loginInd] != data.password) {
+        socket.emit('loginError', 'wrong password');
+      } else {
+        socket.emit('successAuth', 'Welcome!');
+      }
+    }
   })
 });
 
